6413ac77b708c3b1e56dbdc0c6efe11d43bce9da
SantiVigna <ynotvigna@gmail.com>
2025-02-12 10:47:40 +0100

    feat: test Crud for ShoppingList

---
+++ tests/Feature/Controllers/Api/ShoppingListTest.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Tests\Feature\Controllers\Api;
+
+use Tests\TestCase;
+use Database\Seeders\DatabaseSeeder;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Container\Attributes\Database;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ShoppingListTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_CheckIfCanGetAllTheProductsFromShoppingList()
+    {
+        $this->seed(DatabaseSeeder::class);
+
+
+        $response = $this->getJson(route('shoppingListIndex'));
+
+        $response->assertStatus(200)
+                 ->assertJsonCount(12);
+    }
+
+    public function test_CheckIfCanCreateAProductInShoppingList()
+    {   
+        $response = $this->postJson(route('shoppingListStore'), [
+            'name' => 'Test Product',
+            'price' => 10.00
+        ]);
+
+        $response->assertStatus(201)
+                 ->assertJson([
+                     'name' => 'Test Product',
+                     'price' => 10.00
+                 ]);
+    }
+
+    public function test_CheckIfCanGetJustOneProductFromShoppingList() {
+        $this->seed(DatabaseSeeder::class);
+
+        $response = $this->getJson(route('shoppingListShow', 1));
+        
+        $data = ['id' => 1];
+        $response->assertStatus(200)
+                 ->assertJsonFragment($data);
+    }
+
+    public function test_CheckIfCanUpdateAProductInShoppingList() {
+        $this->seed(DatabaseSeeder::class);
+
+        $response = $this->putJson(route('shoppingListUpdate', 1), [
+            'name' => 'La Leche',
+            'price' => 999.00
+        ]);
+
+        $response->assertStatus(200)
+                 ->assertJson([
+                     'name' => 'La Leche',
+                     'price' => 999.00
+                 ]);
+    }
+
+    public function test_CheckIfCanDeleteAProductFromShoppingList() {
+        $this->seed(DatabaseSeeder::class);
+
+        $response = $this->deleteJson(route('shoppingListDestroy', 1));
+
+        $response->assertStatus(200)
+                 ->assertJson([
+                     'message' => 'Product from ShoppingList deleted Succesfully'
+                 ]);
+    }
+}
