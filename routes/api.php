401a397ba27b6505fd8b0fcec4ac49e13c21270f
SantiVigna <ynotvigna@gmail.com>
2025-02-12 09:38:40 +0100

    feat: api routes for shopping lists crud

--- routes/api.php
+++ routes/api.php
@@ -5,4 +5,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ShoppingListController;
 
-
+Route::get('/shoppinglists', [ShoppingListController::class, 'index'])->name('shoppingListIndex'); 
+Route::post('/shoppinglists', [ShoppingListController::class, 'store'])->name('shoppingListStore');
+Route::get('/shoppinglists/{id}', [ShoppingListController::class, 'show'])->name('shoppingListShow');
+Route::delete('/shoppinglists/{id}', [ShoppingListController::class, 'destroy'])->name('shoppingListDestroy');    
+Route::put('/shoppinglists/{id}', [ShoppingListController::class, 'update'])->name('shoppingListUpdate'); 
